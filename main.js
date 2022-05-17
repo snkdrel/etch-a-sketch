0971781385c1662b893947a385308fbebfac6c66
snkdrel <ces.zop@gmail.com>
2022-05-17 17:10:15 -0500

    Change styles to style.css

--- main.js
+++ main.js
@@ -15,24 +15,11 @@ for(let i = 0; i < sides; i++){
     rowContainer.appendChild(currentRow);
 }
 
-// add flexbox to containers
-rowContainer.style.display = 'flex';
-rowContainer.style.cssText = 'flex-direction: column;';
-
-const rows = document.querySelectorAll('.row');
-rows.forEach( (r) => {
-    r.setAttribute('style', 'display: flex;');
-});
-
-const cells = document.querySelectorAll('.cell');
-cells.forEach( (c) => {
-    c.style.cssText = 'width:auto; width: 40px; height: 40px;';
-});
-
 // Change color when hovering
+const cells = document.querySelectorAll('.cell');
+
 cells.forEach( (c) => {
     c.addEventListener('mouseover', function(){
-        console.log(this);
         this.style.backgroundColor = 'black';
     });
 });
