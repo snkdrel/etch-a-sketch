613170250c51545546345b67dfa4deacdd7d8ea8
snkdrel <ces.zop@gmail.com>
2022-05-17 13:58:21 -0500

    Create grid of 16 by 16 cells
    Use divs for cells and containers,using flexbox. Add 'test' text for
    each cell to visualize correct positioning.

--- main.js
+++ main.js
@@ -2,6 +2,23 @@ const rowContainer = document.querySelector('.row-container');
 
 const sides = 16;
 
+// create grid
 for(let i = 0; i < sides; i++){
-
+    const currentRow = document.createElement('div');
+    currentRow.classList.add('row');
+    // add cells
+    for(let j = 0; j < sides; j++){
+        const currentCell = document.createElement('div');
+        currentCell.classList.add('cell');
+        currentCell.textContent = 'test';
+        currentRow.appendChild(currentCell);
+    }
+    rowContainer.appendChild(currentRow);
 }
+
+// add flexbox to containers
+rowContainer.style.display = 'flex';
+rowContainer.style.cssText = 'flex-direction: column';
+
+const rows = document.querySelectorAll('.row');
+rows.forEach( (r) => r.setAttribute('style', 'display: flex;') );
